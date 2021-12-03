53c6c3f0f871f7deec5ab5894af1710774f55f88
Pedro Bernardo <pedrovbernardo@gmail.com>
2021-12-03 14:31:12 -0300

    feat: add toggler component

---
+++ src/js/toggler/constants.js
@@ -0,0 +1,5 @@
+export const NAME = 'Toggler'
+
+export const CSS_CLASSES = {
+  OPEN_CLASS: '-open'
+}
---
+++ src/js/toggler/index.js
@@ -0,0 +1,109 @@
+import {
+  NAME,
+  CSS_CLASSES
+} from './constants'
+
+import { resolveReference } from '../base/resolve-reference'
+import { resolveInstance } from '../base/resolve-instance'
+
+export function Toggler (reference, props = {}) {
+  const trigger = resolveReference(reference, {
+    name: NAME,
+    root: props.root
+  })
+
+  return resolveInstance({
+    props,
+    reference: trigger,
+    factory: TogglerFactory
+  })
+}
+
+function TogglerFactory (trigger, props) {
+  /**
+   * @todo
+   * Implement custom events:
+   * `toggler:<event>`
+   * eg.
+   * toggler:show
+   * toggler:hide
+   * toggler:change
+   */
+
+  const instance = {
+    trigger,
+    content: null,
+    toggle,
+    show,
+    hide,
+    destroy
+  }
+
+  mount()
+
+  trigger[`_${NAME}`] = instance
+
+  return instance
+
+  // ---------------------------------
+  // 🔒 Métodos Privados
+  // ---------------------------------
+
+  function mount () {
+    instance.content = props.content
+
+    createToggler()
+    addTriggers()
+  }
+
+  function unmount () {
+    instance.trigger.removeEventListener('click', onClick)
+    instance.trigger.removeEventListener('click', onKeydown)
+
+    delete instance.trigger[`_${NAME}`]
+
+    for (const propertyName of Object.getOwnPropertyNames(instance)) {
+      instance[propertyName] = null
+    }
+  }
+
+  function createToggler () {
+    instance.trigger.setAttribute('tabIndex', '0')
+  }
+
+  function addTriggers () {
+    instance.trigger.addEventListener('click', onClick)
+    instance.trigger.addEventListener('keydown', onKeydown)
+  }
+
+  function onClick () {
+    toggle()
+  }
+
+  function onKeydown (event) {
+    if (event.code !== 'Space') return
+
+    event.preventDefault()
+    toggle()
+  }
+
+  // ---------------------------------
+  // 🔑 Métodos Públicos
+  // ---------------------------------
+
+  function toggle () {
+    instance.content.classList.toggle(CSS_CLASSES.OPEN_CLASS)
+  }
+
+  function show () {
+    instance.content.classList.add(CSS_CLASSES.OPEN_CLASS)
+  }
+
+  function hide () {
+    instance.content.classList.remove(CSS_CLASSES.OPEN_CLASS)
+  }
+
+  function destroy () {
+    unmount()
+  }
+}
