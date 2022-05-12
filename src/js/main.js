aebd14f7ca80cf01d6e633f3e0b9584ad3b65a77
Pedro Bernardo <pedrovbernardo@gmail.com>
2022-05-12 10:17:16 -0300

    feat: expose new components

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,16 @@ import { Segment } from './segment/index'
 import { Switch } from './switch/index'
 import { Collapse } from './collapse/index'
 import { Toggler } from './toggler/index'
+import { ButtonRadio } from './button-radio/index'
+import { RadioCard } from './radio-card/index'
+import { SelectCard } from './select-card/index'
 
 export default {
   Segment,
   Switch,
   Collapse,
-  Toggler
+  Toggler,
+  ButtonRadio,
+  RadioCard,
+  SelectCard
 }
