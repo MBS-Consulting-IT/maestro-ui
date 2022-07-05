352f1d7ff1a11f35421957d36a89965ab8433ba1
Pedro Bernardo <pedrovbernardo@gmail.com>
2022-07-05 11:12:38 -0300

    refactor/collapse: change active modifier class to '-open'

--- src/js/collapse/constants.js
+++ src/js/collapse/constants.js
@@ -4,6 +4,6 @@ export const CSS_CLASSES = {
   CONTAINER_CLASS: 'o-collapse',
   ITEM_CLASS: 'o-collapse-item',
   ITEM_HEADER_CLASS: 'o-collapse-header',
-  ACTIVE_CLASS: '-active',
+  ACTIVE_CLASS: '-open',
   DISABLED_CLASS: '-disabled'
 }
